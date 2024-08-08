db55a8d489c9971ad083f3920b3a2428975aace4
igriswold <igriswold@uiowa.edu>
2024-08-08 14:39:13 -0500

    functional api calls

---
+++ bs_api_django_app/player_info/api/make_requests.py
@@ -0,0 +1,103 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+from dotenv import load_dotenv
+from typing import Dict, List, Any
+
+class Api_Request:
+    def __init__(self):
+        self.method: str = 'get'
+        self.BASE_URL: str = 'https://api.brawlstars.com/v1/'
+        load_dotenv()
+        self.API_KEY: str = os.getenv('BS_API_KEY')
+        self.player_tag: str = ''
+        self.endpoint: str = f'players/%23{self.player_tag}'
+
+        self.headers: Dict[str, str] = {
+          'Authorization': f'Bearer {self.API_KEY}',
+           'Accept': 'application/json'
+        }
+
+    # Calls BS Api
+    # Returns JSON object helper function
+    def call_api(self, endpoint: str):
+        response = requests.get(self.BASE_URL + endpoint, headers=self.headers)
+
+        if response.status_code == 200:
+            data = response.json()
+    
+            return data
+        else:
+            raise Exception(f"Failed to retrieve data: {response.status_code}\n{response.text}")
+
+    # Returns a JSON object
+    def get_player_info(self) -> Dict[str, Any]:
+        player_endpoint: str = self.get_player_info_endpoint()
+        return self.call_api(player_endpoint)
+    
+    def get_general_brawler_info(self) -> List[Dict[str, Any]]:
+        brawlers_endpoint: str = 'brawlers'
+        brawlers_json = self.call_api(brawlers_endpoint)
+        return brawlers_json.get('items')
+        
+    def get_all_brawlers_info(self) -> List[Dict[str, Any]]:
+        player_data: Dict[str, Any] = self.get_player_info()
+        return player_data.get('brawlers', [])
+    
+    def get_brawler_info(self, brawler_name: str) -> Dict[str, Any]:
+        brawlers: List[Dict[str, Any]] = self.get_all_brawlers_info()
+        for brawler in brawlers:
+            if brawler.get('name') == brawler_name.upper():
+                return brawler
+            
+        raise Exception(f"Could not find {brawler_name}")
+
+    
+    def get_player_info_endpoint(self) -> str:
+        return f'players/%23{self.player_tag}'
+    
+    def set_endpoint(self, endpoint):
+        self.endpoint = endpoint
+        
+    def set_player_tag(self, tag):
+        self.player_tag = tag
+        self.set_endpoint(tag)
+    
+    def get_player_tag(self) -> str:
+        return self.player_tag
+    
+    def get_total_gears(self) -> int:
+        gear_sum: int = 0
+        brawlers: List[Dict[str, Any]] = self.get_all_brawlers_info()
+        for brawler in brawlers:
+            gear_sum += len(brawler.get('gears'))
+        return gear_sum
+    
+    def get_total_star_powers(self) -> int:
+        star_power_sum: int = 0
+        brawlers: List[Dict[str, Any]] = self.get_all_brawlers_info()
+        for brawler in brawlers:
+            star_power_sum += len(brawler.get('starPowers'))
+        return star_power_sum
+    
+    def get_total_gadgets(self) -> int:
+        gadget_sum: int = 0
+        brawlers: List[Dict[str, Any]] = self.get_all_brawlers_info()
+        for brawler in brawlers:
+            gadget_sum += len(brawler.get('gadgets'))
+        return gadget_sum
+    
+    def get_total_brawlers(self) -> int:
+        return len(self.get_general_brawler_info())
+        
+    
+    def get_max_star_powers(self) -> int:
+        star_power_total: int = self.get_total_brawlers() * 2
+        return star_power_total
+    
+    # Currently exact same logic as max star powers
+    # Seperate functions in case third star powers or gadgets are added
+    def get_max_gadgets(self) -> int:
+        gadget_total: int = self.get_total_brawlers() * 2
+        return gadget_total
+        
--- bs_api_django_app/player_info/views.py
+++ bs_api_django_app/player_info/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Player
+from player_info.api.make_requests import Api_Request
 
 def player_info(request):
     myplayers = Player.objects.all().values()
@@ -11,9 +12,12 @@ def player_info(request):
     return HttpResponse(template.render(context, request))
 
 def details(request, id):
+    playerapi = Api_Request()
     myplayer = Player.objects.get(id=id)
+    playerapi.set_player_tag(myplayer.player_id)
     template = loader.get_template('details.html')
     context = {
         'myplayer': myplayer,
+        'playerapi': playerapi,
     }
     return HttpResponse(template.render(context, request))
