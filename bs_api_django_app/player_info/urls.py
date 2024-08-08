e38ea71e0840afeb12355f3efa6f7e2c455ab045
igriswold <igriswold@uiowa.edu>
2024-08-08 14:53:22 -0500

    added main page

--- bs_api_django_app/player_info/urls.py
+++ bs_api_django_app/player_info/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('player_info/', views.player_info, name='player_info'),
     path('player_info/details/<int:id>', views.details, name='details'),
 ]
